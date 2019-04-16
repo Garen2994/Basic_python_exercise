b3b36768346358100c428b059a75e861da6b7ad6
Garen Hou <hjl52994@sina.com>
2019-04-16 20:41:56 +0800

    提取相应位置序列 for 宝宝

--- seq.py
+++ seq.py
@@ -5,29 +5,29 @@
 
 import re
 #把文件处理成单行
-fr=open(r'C://Users//Administrator//Desktop//contigs.txt','r')
-fw=open(r'C://Users//Administrator//Desktop//c1.txt','w') 
-line=fr.read()
-r=line.replace('\n','')
-s=re.sub('>','\n>',r)
+fr = open(r'C://Users//Administrator//Desktop//contigs.txt','r')
+fw = open(r'C://Users//Administrator//Desktop//c1.txt','w') 
+line = fr.read()
+r = line.replace('\n','')
+s = re.sub('>','\n>',r)
 fw.write(s)
 fr.close()
 fw.close()
 
-fr=open(r'C://Users//Administrator//Desktop//blastx_gp60_vs_contigs.txt','r')
-fa=open(r'C://Users//Administrator//Desktop//c1.txt','r')#此处读取上面处理过的文件
-fw_1=open(r'C://Users//Administrator//Desktop//2.txt','w')#写入
-line_fr=fr.readlines()
-line_fa=fa.readlines()
-print(line_fa)
+fr = open(r'C://Users//Administrator//Desktop//blastx_gp60_vs_contigs.txt','r')
+fa = open(r'C://Users//Administrator//Desktop//c1.txt','r')#此处读取上面处理过的文件
+fw_1 = open(r'C://Users//Administrator//Desktop//2.txt','w')#写入
+line_fr = fr.readlines()
+line_fa = fa.readlines()
+#print(line_fa)
 for eachline in line_fr:
-	sp=eachline.strip().split('\t')
+	sp = eachline.strip().split('\t')
 	#print(sp)
-	title=sp[0]
+	title = sp[0]
 	print(title)
-	start=int(sp[7])
+	start = int(sp[7])
 	#print(start)
-	end=int(sp[6])
+	end = int(sp[6])
 	#print(end)
 
 	for each_seq in line_fa:
